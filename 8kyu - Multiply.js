d1d428cca38048338790809e075f6dc0de5ba148
Graziele Dantas <74679475+Trolleza@users.noreply.github.com>
2021-04-08 10:10:33 -0300

    Add files via upload

---
+++ 8kyu - Multiply.js
@@ -0,0 +1,18 @@
+// Multiply
+
+// This code does not execute properly. Try to figure out why.
+
+// const assert = require("chai").assert;
+
+// describe("Multiply", () => {
+//   it("fixed tests", () => {
+//     assert.strictEqual(multiply(1,1), 1);
+//     assert.strictEqual(multiply(2,1), 2);
+//     assert.strictEqual(multiply(2,2), 4);
+//     assert.strictEqual(multiply(3,5), 15);   
+//   });
+// });
+
+const multiply = (a, b) => {
+  return a * b
+};
