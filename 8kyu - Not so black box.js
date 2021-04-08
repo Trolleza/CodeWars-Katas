d9b5b667ecdd98bedad281b7d50f212764394e1f
Graziele Dantas <74679475+Trolleza@users.noreply.github.com>
2021-04-08 10:03:17 -0300

    Add files via upload

---
+++ 8kyu - Not so black box.js
@@ -0,0 +1,13 @@
+// The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+// You may need to think laterally!
+
+// describe("Did you open the box?", function() {
+ 
+//     Test.expect(check(), "It's clamped shut!");
+   
+// });
+
+//ANSEWER:
+//Do console.log(blackBox.toString());
+JSopenSesame();
