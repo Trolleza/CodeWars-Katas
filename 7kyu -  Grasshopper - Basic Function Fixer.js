33f8cb7922db2bb638a5ff04d5f6da8c5cabb42f
Graziele Dantas <74679475+Trolleza@users.noreply.github.com>
2021-08-03 14:58:57 -0300

    Add files via upload

---
+++ 7kyu -  Grasshopper - Basic Function Fixer.js
@@ -0,0 +1,30 @@
+// 7kyu -  Grasshopper - Basic Function Fixer
+
+// Fix the function
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+// Sample Tests:
+// Test.assertEquals(addFive(5), 10)
+// Test.assertEquals(addFive(0), 5)
+// Test.assertEquals(addFive(-5), 0)
+
+function addFive(num) {
+  var total = num + 5;
+  return total;
+}
+
+//OU
+
+function addFive(num) {
+  return num + 5;
+}
+
+//OU
+
+const addFive = (n) => n + 5;
+
+//OU
+
+const addFive = (num) => num + 5;
